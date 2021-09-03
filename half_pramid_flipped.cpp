8e07552505bb31d3656c9970c4d64fc37f0711ee
Shripad <shripad1020@gmail.com>
2021-09-03 11:16:08 +0530

    Half Pyramids Patterns

---
+++ half_pramid_flipped.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	printf("Enter N = ");
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j < n-i-1) {
+				printf("  ");
+			} else {
+				printf("* ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
